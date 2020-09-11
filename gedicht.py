9299d6dcf1acbdc42b25f5415e7534974621f300
michelle frankfort <j.s.frankfort@gmail.com>
2020-09-11 14:37:56 +0200

    added file gedicht.py

---
+++ gedicht.py
@@ -0,0 +1,23 @@
+vers1 = "Ik ben michelle simone Frankfort."
+vers2 = "En ik schrijf dit met slaaptekort."
+vers3 = "Ben nu 22 jaar."
+vers4 = "En heb super awesome haar."
+vers5 = "Ik woon nu in zwaag op een trainingscentrum"
+vers6 = "Rpg's, mmo's, horror en vn zijn mijn game genre's in een vacuüm."
+vers7 = "Waar ben ik nu ook alweer gebleven?"
+vers8 = "En ik hoop dit jaar op een mooie schoolleven."
+vers9 = "Heb nog een vader en broer."
+vers10 = "En aan me moeders kant is bijna iedereen een boer."
+vers11 = "Jammer genoeg is vorig jaar me moeder overleden."
+vers12 = "En dat waren al me gezinsleden."
+vers13 = "Progameren wil ik leren."
+vers14 = "Daarom ben ik dit gedicht aan het manipuleren."
+vers15 = "Ik weet niet meer wat ik moet typen."
+vers16 = "Maar ik wil me nu wel graag in meer stof verdiepen."
+vers17 = "Dit gedicht wordt nu best lang."
+vers18 = "En ik wordt heel bang."
+vers19 = "Hoop dat dit gedicht is goed gelukt."
+vers20 = "Want ben vaak bang voor als iets mislukt."
+
+print(vers1,vers2,vers3,vers4,vers5,vers6,vers7,vers8,vers9,vers10,vers11,vers12,vers13,vers14,vers15,vers16,vers17,vers18,vers19,vers20)
+
