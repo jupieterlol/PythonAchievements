41c97564c818ffde3de7dd3e7e5085c6ee7b538a
michelle frankfort <j.s.frankfort@gmail.com>
2020-09-11 15:07:16 +0200

    added file myFirstOperations

---
+++ myFirstOperations.py
@@ -0,0 +1,17 @@
+print(152 % 11)
+print(15 + 2)
+print(5 / 2) 
+print(18 - 5)
+x = 1
+x += 1
+print(x)
+x -= 1
+print(x)
+x += 100
+print(x)
+x *= 5
+print(x)
+x /= 5
+print(x)
+print(8 // 3)
+print(4 ** 4)
