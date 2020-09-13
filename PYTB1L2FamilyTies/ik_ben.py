6f143cae283de199e0457341fb4776513e12ec89
michelle frankfort <j.s.frankfort@gmail.com>
2020-09-13 22:43:12 +0200

    created ik_ben.py

---
+++ PYTB1L2FamilyTies/ik_ben.py
@@ -0,0 +1,35 @@
+# mijn naam.
+naam = "Michelle"
+
+# mijn leeftijd.
+leeftijd = '22'
+
+# mijn woonplaats
+woonplaats = "zwaag"
+
+# hoeveel kilometer er tussen mij woonplek en school in zit.
+afstandSchool = "40.1 km"
+
+# mijn hobby's in een lijst.
+hobby = ["gamen", "zwemmen", "muziek luisteren", "coderen", "afspreken met vrienden"]
+
+# random dier me telkens zonder reden te binnen schiet.
+randomDier = "penguin"
+
+# de opleiding die ik volg opdit moment.
+opleiding = "software developer"
+
+# de school waar ik op zit.
+school = "media college"
+
+# mijn favoriete game genre's die ik het meeste speel.
+gameGenre = ["horror", "rpg", "mmo", "adventure", "idle"]
+
+
+
+print("Mijn naam is " + naam + ", ik ben " + leeftijd + " jaar oud en ik woon in " + woonplaats + ".")
+print("Ik woon " + afstandSchool + " weg van school.")
+print("Mijn hobby's zijn : " + hobby[0] + ", " + hobby[1] + ", " + hobby[2] + ", " + hobby[3] + ", " + hobby[4] + ".")
+print("Mijn hoofd vindt blijkbaar het dier " + randomDier + " leuk, want het komt ieder keer weer naar voren als ik met iets bezig ben.")
+print("Opdit moment volg ik de opleiding " + opleiding + " op het " + school + ".")
+print("Mijn favoriete game genre's zijn opdit moment : " + gameGenre[0] + ", " + gameGenre[1] + ", " + gameGenre[2] + ", " + gameGenre[3] + ", " + gameGenre[4] + ".")
