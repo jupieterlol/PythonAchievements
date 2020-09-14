170f261f71ce12cd90cbb12d80cca767e2fca50f
michelle frankfort <j.s.frankfort@gmail.com>
2020-09-14 12:54:48 +0200

    Created trust_variables.py

---
+++ PYTB1L2InVariablesWeTrust/trust_variables.py
@@ -0,0 +1,25 @@
+# just a single character
+char = 'k'
+
+# just a float value
+float = 0.3
+
+# booleanCheck references boolean's value
+boolean = True
+booleanCheck = boolean
+
+# lowers the value of intA by value of intB
+intA = 89
+intB = 123
+intAB = intA - intB
+
+# strAB combines words from strA and strB in a sentence
+strA = "Hallo"
+strB = " Teacher"
+strAB = strA + strB
+
+print(char)
+print(float)
+print(booleanCheck)
+print(intAB)
+print(strAB)
