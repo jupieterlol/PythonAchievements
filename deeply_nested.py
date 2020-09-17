aad3628cef0327619572a3f1c881c96a789d0147
michelle frankfort <j.s.frankfort@gmail.com>
2020-09-17 16:00:27 +0200

    updated canWaitForFood and added some comments

--- deeply_nested.py
+++ deeply_nested.py
@@ -1,25 +1,42 @@
+# functions
 def goToStore():
- print("It's smart to get food from the store")
+ print("It's smart to get food from the store.")
 def goOrderOut():
- print("You can afford to order food")
+ print("You can afford to order food.")
 def notHungery():
- print("Not hungery right now")
+ print("Not hungery right now.")
+
 # how hungery you are
 hunger = 45
+
 # time last eaten in min
 timeLastEaten = 350
+
 # money in cents
 money = 2000
+
+# is it weekend
 weekend = True
+
+# can you miss money at the moment
 canMissMoney = True
+
 # do you want to cook
 cook = False
-if timeLastEaten > 599:
+
+# will you have vistors over soon
+vistorsSoon = false
+
+# do you need to do something soon.
+noTime = false
+
+#check if you can wait for food
+if timeLastEaten > 599 or vistorsSoon == true or noTime == true:
  canWaitForFood = False
 else:
  canWaitForFood = True
  
-
+# check if you need food and if you can afford to order out.
 if hunger > 50 or timeLastEaten > 300:
  if weekend or canMissMoney:
   if money >= 2000:
