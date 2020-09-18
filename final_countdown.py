daf087ef982dcd8cb454c4f15051706513266a6f
michelle frankfort <j.s.frankfort@gmail.com>
2020-09-18 15:05:40 +0200

    Added time and sleep to count it down per seconds

--- final_countdown.py
+++ final_countdown.py
@@ -1,5 +1,7 @@
 import webbrowser
-for x in range(1000,-1,-1):
+import time
+for x in range(10,-1,-1):
  print(x)
+ time.sleep(1)
 if x == 0:
  webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstleyVEVO', new=2)
